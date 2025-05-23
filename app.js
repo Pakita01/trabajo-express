ef8a28dd5308e7e02bf3e6e336c533ac1dd4a93a
Pakita01 <pakitastudio@gmail.com>
2025-05-23 11:05:21 -0400

    Colocacion del metodo delete

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ app.get('/', (req,res) => {
     res.send('mi primer servidor. cursos 🐱‍👤')
 })
 
+// DELETE: Eliminar curso de inglés por ID
+routerIngles.delete('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const indice = infoCurso.ingles.findIndex(curso => curso.id === id);
+
+  if (indice === -1) {
+    return res.status(404).send('Curso no encontrado.');
+  }
+
+  const cursoEliminado = infoCurso.ingles.splice(indice, 1);
+  res.json(cursoEliminado);
+});
+
+
+
+
 const PUERTO = process.env.PORT || 3000;
+
 
 app.listen(PUERTO, () => {
     console.log('el servidor esta iniciando procesos en el puerto ${PUERTO}...')
